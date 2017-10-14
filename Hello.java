f8957da13c216221751f0138d15b7927de0fdc42
uttambhagaur <uttambhagaur@gmail.com>
2017-10-14 18:54:30 +0530

    Add Hello program

---
+++ Hello.java
@@ -0,0 +1,13 @@
+/*
+Written by: Uttam Singh
+Date : 14/10/2017
+*/
+// How to compile and run java package on cmd
+// javac -d . Hello.java
+//java examples/Hello
+package examples;
+public class Hello{
+	public static void main(String[] args) {
+		System.out.println(  "  Hello World!");
+	}
+}
